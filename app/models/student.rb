9e0ccf63330b013b4c4f178e66e34b90df5642ea
Andre Pereira <andreporteira@gmail.com>
2023-07-13 15:36:58 -0300

    Fixed bug home and activity

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,13 @@
 class Student < ApplicationRecord
+  # before_destroy :destroy_activities
+  has_many :activities, dependent: :destroy
   belongs_to :classroom
   enum status: { 'Matriculado': 1, 'Não matriculado': 2 }
+  validates_uniqueness_of :name, scope: :classroom
+
+  private
+
+  # def destroy_activities
+  #   self.Activity.destroy_all
+  # end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,4 @@
 class Teacher < ApplicationRecord
-  has_many :classrooms
+  has_many :classrooms, dependent: :destroy
   enum status: { 'disponível': 1, 'não disponível': 2 }
 end
