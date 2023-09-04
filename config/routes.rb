8d7a0482c0227509057a163a5632f7d467895423
Andre Pereira <andreporteira@gmail.com>
2023-09-04 17:03:10 -0300

    CPF no User_devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { 
+    sessions: 'users/sessions',
+    registrations: 'users/registrations' }
   resources :activities
   resources :students
   resources :classrooms
   resources :teachers
+  get 'users/search/index'
   get 'search/index'
   get 'devise/search/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
