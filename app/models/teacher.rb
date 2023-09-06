fab803693e3dabf40ce56aa1004d10288eda15c6
Andre Pereira <andreporteira@gmail.com>
2023-09-06 15:04:08 -0300

    Classrooms por Relação

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,5 @@
 class Teacher < ApplicationRecord
-  belongs_to :user
+
   has_many :classrooms, dependent: :destroy
   validates_presence_of :name
   enum status: { 'disponível': 1, 'não disponível': 2 }
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "englishglobe-andreporteira.b4a.run"
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -10,7 +10,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  
   # Show full error reports.
   config.consider_all_requests_local = true
 
