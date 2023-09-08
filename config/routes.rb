914e9989de7c315d4f4ca6018df96beb58c86988
Andre Pereira <andreporteira@gmail.com>
2023-09-08 10:49:04 -0300

    Views configuradas usando relações e novo role criado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { 
+  get 'users/index'
+ 
+  devise_for :users, :path_prefix => 'id', controllers: { 
     sessions: 'users/sessions',
-    registrations: 'users/registrations' }
+    registrations: 'users/registrations'  }
+    resources :users, :only =>[:show]
   resources :activities
   resources :students
   resources :classrooms
@@ -9,7 +12,10 @@ Rails.application.routes.draw do
   resources :users
   get 'search/index'
 
+  match '/users',   to: 'users#index',   via: 'get'
+ 
   
+  match '/users/:id',     to: 'users#show',       via: 'get'
   # get 'devise/search/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
