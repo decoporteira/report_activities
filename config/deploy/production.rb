c3b86af7a50a40a7979ca2a8cd275d77c3cbdb66
Andre Pereira <andreporteira@gmail.com>
2023-09-14 18:49:23 -0300

    Change ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '165.227.81.161', user: 'deploy', roles: %w{app db web}
+server '142.93.6.208', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
