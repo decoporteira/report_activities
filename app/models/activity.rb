36cd00bbd338b11930efee3b79552b807c5a1934
Andre Pereira <andreporteira@gmail.com>
2023-09-20 14:09:06 -0300

    Opção Ausente

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ApplicationRecord
   belongs_to :student
-  enum late: { feito: 0, 'entregue com atraso': 1, 'não fez': 2 }
+  enum late: { feito: 0, 'entregue com atraso': 1, 'não fez': 2, ausente: 3 }
   validates_presence_of :date, :late, :student_id
 end
