db1e0a1211cf9e8a5f24c1a9e28544d2d7feda1a
Andre Pereira <andreporteira@gmail.com>
2023-09-20 14:37:36 -0300

    Classes com nome de professor

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -2,4 +2,8 @@ class Classroom < ApplicationRecord
   belongs_to :teacher
   has_many :students, dependent: :destroy
   validates_presence_of :name, :time, :teacher_id
+
+  def name_with_teacher_name
+    "#{name} - #{teacher.name}"
+  end
 end
