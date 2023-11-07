cef2d400c903a31f8d94c30c346cf1aa62d08b0d
Andre Pereira <andreporteira@gmail.com>
2023-11-07 08:25:30 -0300

    ActionMailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'englishglobe.com.br', # Remove trailing ‘/’
+    user_name: 'decodesiste@gmail.com', # Replace with your Gmail email
+    password: 'fjxj wvlo fsmz fqjm', # Replace with the App Password you generated
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5
+}
