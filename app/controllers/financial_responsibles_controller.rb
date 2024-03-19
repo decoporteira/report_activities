f6a30c008a9f709d1d962229bd234daa456fb7ba
André Pereira <andreporteira@gmail.com>
2024-03-19 17:17:16 -0300

    Adiciona edição de resposnáveis

--- app/controllers/financial_responsibles_controller.rb
+++ app/controllers/financial_responsibles_controller.rb
@@ -1,4 +1,5 @@
 class FinancialResponsiblesController < ApplicationController
+  before_action :set_responsible, only: %i[show edit update]
 
   def index
     @financial_responsibles = FinancialResponsible.all
@@ -18,12 +19,27 @@ class FinancialResponsiblesController < ApplicationController
     end
   end
 
-  def show
+  def show; end
+
+  def edit
+  end
+
+  def update
+    if @financial_responsible.update(financial_responsible_params)
+      redirect_to @financial_responsible, notice: 'Responsável editado com sucesso.'
+    else
+      flash[:alert] = 'Não foi possível editar o Responsável.'
+      render :edit
+    end
   end
 
   private
 
   def financial_responsible_params
     params.require(:financial_responsible).permit(:name, :phone, :cpf, :email)
+  end
+
+  def set_responsible
+    @financial_responsible = FinancialResponsible.find(params[:id])
   end
 end
