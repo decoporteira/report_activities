25acc09fc799d4ca920f74cc73b8027549f77d3c
André Pereira <andreporteira@gmail.com>
2024-04-08 13:48:23 -0300

    Conserta bug do cpf

--- spec/system/financial_responsibles/users_creates_responsible_spec.rb
+++ spec/system/financial_responsibles/users_creates_responsible_spec.rb
@@ -23,6 +23,31 @@ RSpec.describe 'Usuário cria um responsável financeiro' do
     expect(page).to have_content('Responsável foi criado, porém sem nenhum aluno ligado a ele.')
   end
 
+  it 'likando um aluno com sucesso' do
+    user = User.create!(email: 'admin@admin.com.br', password: 'password', role: 'admin')
+    user_teacher = User.create!(email: 'teacher@admin.com.br', password: 'password', role: 'teacher')
+    teacher = Teacher.create!(name: 'Bianca', status: 'disponível', user_id: user_teacher.id,
+                              cpf: '087.097.098-01')
+    classroom = Classroom.create!(name: 'MW 17:00', teacher_id: teacher.id, time: '23:00')
+    Student.create!(name: 'Venossaur', status: 'Matriculado', classroom_id: classroom.id,
+                    cpf: '065.654.654-01')
+
+    login_as(user)
+    visit(root_path)
+    click_on('Alunos')
+    click_on('Detalhes')
+    click_on('Criar Responsável')
+    fill_in 'Nome', with: 'Prof. Oak'
+    fill_in 'CPF', with: '000.000.000-01'
+    fill_in 'Email', with: 'oak@email.com'
+    fill_in 'Telefone', with: '3299900-0000'
+    click_on 'Salvar'
+
+    expect(page).to have_content('Responsável cadastrado com sucesso.')
+  end
+
+  
+
   it 'e falha' do
     user = User.create!(email: 'admin@admin.com.br', password: 'password', role: 'admin')
     user_teacher = User.create!(email: 'teacher@admin.com.br', password: 'password', role: 'teacher')
