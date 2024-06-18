bda7f04128d8770981da6b0307e2f947e257abfc
André Pereira <andreporteira@gmail.com>
2024-06-18 10:24:05 -0300

    Testes passando.

--- spec/system/monthly_fees/user_creates_monthly_fee_spec.rb
+++ spec/system/monthly_fees/user_creates_monthly_fee_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'Admin cria taxas mensais' do
 
     fill_in 'Vencimento', with: '2005/04/04'
     fill_in 'Valor', with: '300,00'
-    fill_in 'Status', with: 'Pagamento em atraso'
+    select 'Pagamento Em Atraso', from: 'Status'
 
     expect { click_on 'Criar Mensalidade' }.to change { MonthlyFee.count }.by(1)
     expect(page).to have_content('Mensalidade criada com sucesso.')
@@ -40,7 +40,7 @@ RSpec.describe 'Admin cria taxas mensais' do
 
     fill_in 'Vencimento', with: '2024/04/04'
     fill_in 'Valor', with: '300,00'
-    fill_in 'Status', with: 'Pago'
+    select 'Pago', from: 'Status'
 
     expect { click_on 'Criar Mensalidade' }.to change { MonthlyFee.count }.by(1)
     expect(page).to have_content('Mensalidade criada com sucesso.')
