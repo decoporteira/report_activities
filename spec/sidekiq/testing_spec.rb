f21d8b1f4e9c9b5e6eadccde6d3f35dd1c727af5
André Pereira <andreporteira@gmail.com>
2024-06-24 16:59:42 -0300

    WIP: testes do sidekiq

---
+++ spec/sidekiq/testing_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+require 'sidekiq/testing' 
+
+RSpec.describe DailyJob, type: :job do
+  describe '#perform' do
+    it 'enqueues the job correctly' do
+      Sidekiq::Testing.fake!
+
+      expect {
+        DailyJob.perform_async(1, 2)
+      }.to change(DailyJob.jobs, :size).by(1)
+    end
+
+    it 'executes the job inline' do
+      Sidekiq::Testing.inline! do
+        DailyJob.perform_async(1, 2)
+      end
+
+      # Add expectations here for the job execution, if needed
+    end
+  end
+end
