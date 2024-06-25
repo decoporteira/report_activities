b0408ba17c89d28aa5c5ad5532a9fd24a4c86824
André Pereira <andreporteira@gmail.com>
2024-06-25 09:45:10 -0300

    Retira comentário

--- app/workers/daily_job.rb
+++ app/workers/daily_job.rb
@@ -5,7 +5,6 @@ class DailyJob
     monthly_fees = MonthlyFee.where(status: 'A pagar').where('due_date < ?', Date.today)
     monthly_fees.each do |monthly_fee|
       monthly_fee.update(status: 'Atrasada')
-      puts "Mensalidade de #{monthly_fee.student.name} foi marcada como #{monthly_fee.status} às #{Time.now}"
     end
   end
 end
