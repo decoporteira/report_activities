07dfaddb3673710909e4999f1d340b2156ca7df7
André Pereira <andreporteira@gmail.com>
2024-07-04 14:00:47 -0300

    Adiciona função para marcar atividade como não feita caso o aluno tenha faltado.

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -11,9 +11,16 @@ class AttendancesController < ApplicationController
                                                          attendance_date: params[:items][:date])
     if params[:items][:presence] == 'true'
       attendance_record.update(presence: false)
+      attendance_record.attendance_date
+      student.activities.where(date: attendance_record.attendance_date).each do |activity|
+        activity.update(late: 'não fez')
+      end
       redirect_to classroom_path(classroom), notice: 'Presença marcada como Ausente.'
     else
       attendance_record.update(presence: true)
+      student.activities.where(date: attendance_record.attendance_date).each do |activity|
+        activity.update(late: 'feito')
+      end
       redirect_to classroom_path(classroom), notice: 'Presença marcada como Presente.'
     end
   end
