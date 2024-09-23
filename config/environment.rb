745e3ad57a2789b541c8f466c36d7f8a10a5fefc
André Pereira <andreporteira@gmail.com>
2024-09-23 14:11:10 -0300

    Muda sistema de mails

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,27 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.gmail.com',
+#   port: 587,
+#   domain: 'englishglobe.com.br', # Remove trailing ‘/’
+#   user_name: 'contatoenglishglobe@gmail.com', # Replace with your Gmail email
+#   password: 'wfnb ygii nhki qpfg', # Replace with the App Password you generated
+#   authentication: 'plain',
+#   enable_starttls_auto: true,
+#   open_timeout: 5,
+#   read_timeout: 5
+# }
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain: 'englishglobe.com.br', # Remove trailing ‘/’
-  user_name: 'contatoenglishglobe@gmail.com', # Replace with your Gmail email
-  password: 'wfnb ygii nhki qpfg', # Replace with the App Password you generated
+  address: 'smtp.titan.email',
+  port: 465,
+  domain: 'reports.englishglobe.com.br', # Remove trailing ‘/’
+  user_name: 'no-reply@reports.englishglobe.com.br', # Replace with your Gmail email
+  password: 'Virna1802!', # Replace with the App Password you generated
   authentication: 'plain',
   enable_starttls_auto: true,
   open_timeout: 5,
