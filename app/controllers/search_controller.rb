5174c21423a739ddcbeb0914e3354e7d4f328ab0
André Pereira <andreporteira@gmail.com>
2024-10-03 09:58:11 -0300

    Nova pesquisa

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,19 @@
 class SearchController < ApplicationController
   def index
     query = params[:query].to_s
-    query_sem_acento = substitui_vogais_com_acento(query)
 
     @students =
-      Student.where('LOWER(name) LIKE ?', "%#{query_sem_acento.downcase}%")
+    Student.where(
+      "TRANSLATE(LOWER(name), 'ÁÀÂÃÄáàâãäÉÈÊËéèêëÍÌÎÏíìîïÓÒÕÔÖóòôõöÚÙÛÜúùûüÇç', 
+       'AAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUUuuuuÇc') LIKE ?", 
+       "%#{query.downcase}%"
+    )
     @classrooms =
-      Classroom.where('LOWER(name) LIKE ?', "%#{query_sem_acento.downcase}%")
+      Classroom.where(
+        "TRANSLATE(LOWER(name), 'ÁÀÂÃÄáàâãäÉÈÊËéèêëÍÌÎÏíìîïÓÒÕÔÖóòôõöÚÙÛÜúùûüÇç', 
+         'AAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUUuuuuÇc') LIKE ?", 
+         "%#{query.downcase}%"
+      )
     @teachers = filter_teachers
     @financial_responsibles =
       FinancialResponsible.where(
@@ -18,19 +25,15 @@ class SearchController < ApplicationController
   def filter
     query = params[:query].to_s
     query_sem_acento = substitui_vogais_com_acento(query)
-    @classrooms = Classroom.where('name LIKE ?', "%#{query_sem_acento}%")
+    @classrooms = Classroom.where( "TRANSLATE(LOWER(name), 'ÁÀÂÃÄáàâãäÉÈÊËéèêëÍÌÎÏíìîïÓÒÕÔÖóòôõöÚÙÛÜúùûüÇç', 
+    'AAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUUuuuuÇc') LIKE ?", 
+    "%#{query.downcase}%")
     return if params[:teacher_id].blank?
 
     @classrooms = @classrooms.where(teacher_id: params[:teacher_id])
   end
 
   private
-
-  def substitui_vogais_com_acento(query)
-    query
-      .gsub(/[áàãâäéèêëíìîïóòõôöúùûüÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÔÖÚÙÛÜ]/, '_')
-      .gsub(/[aeiouAEIOU]/, '_')
-  end
 
   def filter_teachers
     teachers =
