fdc753abdaeac74dc91ee45c79ff20c802a1d6db
André Pereira <andreporteira@gmail.com>
2024-12-04 15:34:50 -0300

    Add novo daily job

--- app/workers/daily_job.rb
+++ app/workers/daily_job.rb
@@ -2,8 +2,8 @@ class DailyJob
   include Sidekiq::Job
 
   def perform(*_args)
-    # monthly_fees =
-    #   MonthlyFee.where(status: 'A pagar').where('due_date < ?', Time.zone.today)
-    # monthly_fees.each { |monthly_fee| monthly_fee.update(status: 'Atrasada') }
+    monthly_fees =
+      MonthlyFee.where(status: 'A pagar').where('due_date < ?', Time.zone.today)
+    monthly_fees.each { |monthly_fee| monthly_fee.update(status: 'Atrasada') }
   end
 end
