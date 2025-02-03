0ab775fb32611b2393d7c6d3cf17c470b856ff01
André Pereira <andreporteira@gmail.com>
2025-02-03 11:29:44 -0300

    Cria mensalidades por mês

--- app/workers/create_monthly_fees.rb
+++ app/workers/create_monthly_fees.rb
@@ -10,14 +10,12 @@ class CreateMonthlyFees
       start_month = current_month == 1 ? 2 : current_month
 
       Student.transaction do
-        (start_month..12).each do |month|
-          MonthlyFee.create!(
-            student:,
-            value: student.current_plan.discounted_price,
-            due_date: Date.new(current_year, month, 10),
-            status: 'A pagar'
-          )
-        end
+        MonthlyFee.create!(
+          student:,
+          value: student.current_plan.discounted_price,
+          due_date: Date.new(current_year, current_month, 10),
+          status: 'A pagar'
+        )
       end
     end
   end
