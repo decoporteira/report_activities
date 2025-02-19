b1a049295960dc99c48c347a42dc7fc96d572aa4
André Pereira <andreporteira@gmail.com>
2025-02-19 09:52:22 -0300

    Conserta bug do worker create monthly fees

--- app/workers/create_monthly_fees.rb
+++ app/workers/create_monthly_fees.rb
@@ -10,6 +10,8 @@ class CreateMonthlyFees
       start_month = current_month == 1 ? 2 : current_month
 
       Student.transaction do
+        next if student.monthly_fees.exists?(due_date: Date.new(current_year, current_month, 10))
+
         MonthlyFee.create!(
           student:,
           value: student.current_plan.discounted_price,
