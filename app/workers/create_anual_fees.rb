96fd8fe475142210ca5b2d570ce5e43f28d17c4a
André Pereira <andreporteira@gmail.com>
2025-05-05 16:03:07 -0300

    Conserta js e add criação de todas as mensalidades do ano

---
+++ app/workers/create_anual_fees.rb
@@ -0,0 +1,29 @@
+class CreateAnualFees
+  include Sidekiq::Job
+
+  def perform(*_args)
+    Student.active.find_each(batch_size: 50) do |student|
+      next if student.current_plan.nil?
+
+      current_year = Time.zone.today.year
+      Student.transaction do
+        (6..12).each do |month|
+          next if student.monthly_fees.exists?(due_date: Date.new(current_year, month, 10))
+
+          create_monthly_fee(student, month, current_year)
+        end
+      end
+    end
+  end
+
+  private
+
+  def create_monthly_fee(student, month, current_year)
+    MonthlyFee.create!(
+      student:,
+      value: student.current_plan.discounted_price,
+      due_date: Date.new(current_year, month, 10),
+      status: 'A pagar'
+    )
+  end
+end
---
+++ spec/sidekiq/anual_fees_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+RSpec.describe CreateAnualFees, type: :worker do
+  it 'creates monthly fees for students' do
+    FactoryBot.create(:user, role: 'admin')
+    user_teacher = FactoryBot.create(:user, role: 'teacher')
+    teacher = FactoryBot.create(:teacher, user_id: user_teacher.id)
+    classroom = FactoryBot.create(:classroom, teacher_id: teacher.id)
+
+    student_a = FactoryBot.create(:student, name: 'Venossaur', classroom_id: classroom.id)
+    student_b = FactoryBot.create(:student, name: 'Charmander', classroom_id: classroom.id)
+    student_c = FactoryBot.create(:student, name: 'Squirtle', classroom_id: classroom.id)
+    student_d = FactoryBot.create(:student, name: 'Caterpie', classroom_id: classroom.id)
+    student_e = FactoryBot.create(:student, name: 'Stariu', status: 'unregistered', classroom_id: classroom.id)
+
+    plan_a = FactoryBot.create(:plan, name: 'Kids', price: 300)
+    plan_b = FactoryBot.create(:plan, name: 'Adult', price: 350)
+
+    FactoryBot.create(:current_plan, plan: plan_a, student: student_a)
+    FactoryBot.create(:current_plan, plan: plan_a, student: student_b)
+    FactoryBot.create(:current_plan, plan: plan_b, student: student_c)
+
+    CreateAnualFees.new.perform
+
+    expect(MonthlyFee.count).to eq(21)
+  end
+  it 'creates monthly fees for students when there is a MF already created' do
+    FactoryBot.create(:user, role: 'admin')
+    user_teacher = FactoryBot.create(:user, role: 'teacher')
+    teacher = FactoryBot.create(:teacher, user_id: user_teacher.id)
+    classroom = FactoryBot.create(:classroom, teacher_id: teacher.id)
+
+    student_a = FactoryBot.create(:student, name: 'Venossaur', classroom_id: classroom.id)
+    student_b = FactoryBot.create(:student, name: 'Charmander', classroom_id: classroom.id)
+    student_c = FactoryBot.create(:student, name: 'Squirtle', classroom_id: classroom.id)
+    student_d = FactoryBot.create(:student, name: 'Caterpie', classroom_id: classroom.id)
+    student_e = FactoryBot.create(:student, name: 'Stariu', status: 'unregistered', classroom_id: classroom.id)
+
+    plan_a = FactoryBot.create(:plan, name: 'Kids', price: 300)
+    plan_b = FactoryBot.create(:plan, name: 'Adult', price: 350)
+
+    FactoryBot.create(:current_plan, plan: plan_a, student: student_a)
+    FactoryBot.create(:current_plan, plan: plan_a, student: student_b)
+    FactoryBot.create(:current_plan, plan: plan_b, student: student_c)
+
+    travel_to Time.zone.local(2025, 5, 5) do
+      CreateAnualFees.new.perform
+    end
+
+    expect(MonthlyFee.count).to eq(21)
+  end
+
+  it 'creates monthly fees for students in the month = 6' do
+    FactoryBot.create(:user, role: 'admin')
+    user_teacher = FactoryBot.create(:user, role: 'teacher')
+    teacher = FactoryBot.create(:teacher, user_id: user_teacher.id)
+    classroom = FactoryBot.create(:classroom, teacher_id: teacher.id)
+
+    student_a = FactoryBot.create(:student, name: 'Venossaur', classroom_id: classroom.id)
+    student_b = FactoryBot.create(:student, name: 'Charmander', classroom_id: classroom.id)
+    student_c = FactoryBot.create(:student, name: 'Squirtle', classroom_id: classroom.id)
+
+    plan_a = FactoryBot.create(:plan, name: 'Kids', price: 300)
+    plan_b = FactoryBot.create(:plan, name: 'Adult', price: 350)
+
+    FactoryBot.create(:current_plan, plan: plan_a, student: student_a)
+    FactoryBot.create(:current_plan, plan: plan_a, student: student_b)
+    FactoryBot.create(:current_plan, plan: plan_b, student: student_c)
+
+    FactoryBot.create(:monthly_fee, student: student_a, due_date: Date.new(2025, 7, 10), status: 'A pagar')
+    travel_to Time.zone.local(2026, 6, 5) do
+      CreateAnualFees.new.perform
+    end
+    expect(MonthlyFee.count).to eq(22)
+  end
+end
