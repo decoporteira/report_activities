d86d4caf6f7c528f3a3b0d4134d0bbd84fd454fb
André Pereira <andreporteira@gmail.com>
2025-05-20 15:06:58 -0300

    Add campo billing_type no model Plan e um enum para controle de tipo de cobrança

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_01_112818) do
+ActiveRecord::Schema[7.0].define(version: 2025_05_20_180403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -67,8 +67,11 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_01_112818) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "total", precision: 10, scale: 2
+    t.decimal "value_per_hour"
+    t.bigint "teacher_id"
     t.index ["plan_id"], name: "index_current_plans_on_plan_id"
     t.index ["student_id"], name: "index_current_plans_on_student_id"
+    t.index ["teacher_id"], name: "index_current_plans_on_teacher_id"
   end
 
   create_table "financial_responsibles", force: :cascade do |t|
@@ -109,6 +112,18 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_01_112818) do
     t.decimal "price", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "plan_type"
+    t.integer "billing_type"
+  end
+
+  create_table "private_lessons", force: :cascade do |t|
+    t.bigint "current_plan_id", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["current_plan_id"], name: "index_private_lessons_on_current_plan_id"
   end
 
   create_table "responsibles", force: :cascade do |t|
@@ -174,8 +189,10 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_01_112818) do
   add_foreign_key "classrooms", "teachers"
   add_foreign_key "current_plans", "plans"
   add_foreign_key "current_plans", "students"
+  add_foreign_key "current_plans", "teachers"
   add_foreign_key "material_billings", "students"
   add_foreign_key "monthly_fees", "students"
+  add_foreign_key "private_lessons", "current_plans"
   add_foreign_key "responsibles", "financial_responsibles"
   add_foreign_key "responsibles", "students"
   add_foreign_key "resumes", "students"
