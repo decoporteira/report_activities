30a05592ce91972fa2d8b6636a397dba7fbd7712
André Pereira <andreporteira@gmail.com>
2025-06-03 09:21:38 -0300

    Listagem de professores com valores das aulas

--- app/controllers/private_classes_controller.rb
+++ app/controllers/private_classes_controller.rb
@@ -1,7 +1,25 @@
 class PrivateClassesController < ApplicationController
 before_action :admin?
   def index
-    @private_lessons = PrivateLesson.all
+    @date = params[:start_date].present? ? Date.parse(params[:start_date]) : Date.today
+    start_of_month = @date.beginning_of_month
+    end_of_month = @date.end_of_month
+
+    @private_lessons = PrivateLesson
+                       .includes(current_plan: [:student, :teacher])
+                       .where(start_time: start_of_month..end_of_month)
+
+    @teachers = Teacher.all
+    @lesson_counts = @private_lessons
+                     .joins(:current_plan)
+                     .group('current_plans.teacher_id')
+                     .count
+
+    @lesson_values = @private_lessons
+                     .joins(:current_plan)
+                     .group('current_plans.teacher_id')
+                     .sum('current_plans.value_per_hour')
+    @teachers = Teacher.where(id: @lesson_counts.keys).index_by(&:id)
   end
 
   private
