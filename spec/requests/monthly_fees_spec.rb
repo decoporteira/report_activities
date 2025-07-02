c215a8811c699294766838f0c5ca54f3f28029bb
André Pereira <andreporteira@gmail.com>
2025-07-02 12:02:38 -0300

    volta com a data de um dia anterior

--- spec/requests/monthly_fees_spec.rb
+++ spec/requests/monthly_fees_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "MonthlyFees", type: :request do
+  include Devise::Test::IntegrationHelpers
+    let(:user) { create(:user) }
   describe "PATCH /monthly_fees/:id/update_paid" do
     let(:student) { create(:student) }
     let(:plan) { create(:plan, :per_class) }
@@ -8,15 +10,16 @@ RSpec.describe "MonthlyFees", type: :request do
     let(:monthly_fee) { create(:monthly_fee, student: student, due_date: Date.current) }
 
     before do
+      sign_in user
       allow_any_instance_of(Student).to receive(:current_plan).and_return(current_plan)
     end
 
     context "when status is 'Paga' and plan is per_class" do
       before do
+        headers = { "HTTP_REFERER" => "/" }
         create_list(:private_lesson, 3, current_plan: current_plan, start_time: 1.month.ago.beginning_of_month + 2.days)
-        patch update_paid_monthly_fee_path, params: {
-          items: { id: monthly_fee.id, status: "Paga" }
-        }
+        patch '/update_paid', params: { items: { id: monthly_fee.id, status: 'Paga', value: 10 } }, headers: headers
+
       end
 
       it "updates the monthly_fee with correct value" do
@@ -31,10 +34,11 @@ RSpec.describe "MonthlyFees", type: :request do
       let(:plan) { create(:plan, :both, price: 100) }
 
       before do
+        headers = { "HTTP_REFERER" => "/" }
         create_list(:private_lesson, 2, current_plan: current_plan, start_time: 1.month.ago.beginning_of_month + 5.days)
-        patch update_paid_monthly_fee_path, params: {
+        patch '/update_paid', params: {
           items: { id: monthly_fee.id, status: "Paga" }
-        }
+        }, headers: headers
       end
 
       it "updates with class total + plan price" do
@@ -45,9 +49,10 @@ RSpec.describe "MonthlyFees", type: :request do
 
     context "when status is not 'Paga'" do
       before do
-        patch update_paid_monthly_fee_path, params: {
+        headers = { "HTTP_REFERER" => "/" }
+        patch '/update_paid', params: {
           items: { id: monthly_fee.id, status: "Atrasada" }
-        }
+        }, headers: headers
       end
 
       it "only updates the status" do
