f9c5ed83511f00645db8931d4bc8cc8f16b8452f
André Pereira <andreporteira@gmail.com>
2025-09-02 16:25:40 -0300

    Add mudança de status por sidekiq

--- app/workers/daily_job.rb
+++
@@ -1,9 +0,0 @@
-class DailyJob
-  include Sidekiq::Job
-
-  def perform(*_args)
-    monthly_fees =
-      MonthlyFee.where(status: 'A pagar').where('due_date < ?', Time.zone.today)
-    monthly_fees.each { |monthly_fee| monthly_fee.update(status: 'Atrasada') }
-  end
-end
